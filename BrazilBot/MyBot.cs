f69be4c1139123e18547a3601e50ec537b44bc71
SirErik <erik9395@hotmail.com>
2017-02-28 00:59:16 +0100

    Update MyBot.cs

--- BrazilBot/MyBot.cs
+++ BrazilBot/MyBot.cs
@@ -1,4 +1,4 @@
-﻿using Discord;
+using Discord;
 using Discord.Commands;
 
 using System;
@@ -12,7 +12,7 @@ namespace BrazilBot
         DiscordClient _client;
         CommandService commands;
 
-        private const string _TOKEN = "InsertTokenHere";
+        private const string _TOKEN = "";
 
         public MyBot()
         {
@@ -33,27 +33,12 @@ namespace BrazilBot
 
             registerCommands();
 
-            _client.UserJoined += async (s, e) => {
+            _client.UserJoined += async (s, e) =>
+            {
                 // Check to make sure that the user is not a bot
                 if (!e.User.IsBot)
                     // Say hola to the user that joined!
-                    await e.Server.FindChannels("bottest").FirstOrDefault().SendMessage($"User joined! Hola {e.User.NicknameMention}!");
-            };
-
-            _client.UserUpdated += async (s, e) =>
-            {
-                int minutes = 10;
-                if (e.After.LastActivityAt != null)
-                {
-                    TimeSpan timeElapsed = DateTime.Now.AddHours(1) - (DateTime)e.After.LastActivityAt; //Calculate how long it has been since user was last online
-                    minutes = Convert.ToInt32(timeElapsed.TotalMinutes); //Convert to minutes
-                    await e.Server.FindChannels("bottest").FirstOrDefault().SendMessage($"{e.After.NicknameMention}'s last activity was at {e.After.LastActivityAt} which was {minutes} minutes ago!");
-                }
-                
-                if ((e.After.Status.Equals(UserStatus.Online) && minutes >= 10) && (e.After.CurrentGame == null || e.Before.CurrentGame == null)) //User has come online, hasn't been online for more than 10 minutes and did not enter/exit game.
-                {
-                    await e.Server.FindChannels("bottest").FirstOrDefault().SendMessage($"Hola {e.After.NicknameMention}!");
-                }
+                    await e.Server.FindChannels("perssons_bil").FirstOrDefault().SendMessage($"Hola {e.User.NicknameMention}! Welcome to The Black WidowZ discord. Post all links in #links and enjoy your stay.");
             };
 
             _client.ExecuteAndWait(async () =>
@@ -61,7 +46,7 @@ namespace BrazilBot
                 await _client.Connect(_TOKEN, TokenType.Bot);
             });
 
-            
+
         }
 
         private void registerCommands()
@@ -81,22 +66,18 @@ namespace BrazilBot
                 {
                     await e.Channel.SendMessage("I am delusional and think I can beat push strat. I do however have the biggest dick.");
                 });
-             commands.CreateCommand("bouncysheep")
-                        .Do(async (e) =>
-                        {
-                            await e.Channel.SendMessage("The most bounciest bouncer of the bouncy sheep.");
-                        });
             commands.CreateCommand("#")
                 .Do(async (e) =>
                 {
-                    if (e.User.Name.Equals("`HashTag`"))
+                    if (e.User.Name.Equals("`Hash_Tag`"))
                     {
                         await e.Channel.SendMessage("#Sweet!");
-                    }else
+                    }
+                    else
                     {
                         await e.Channel.SendMessage($"#WhatAPlayer {e.User.NicknameMention}!");
                     }
-                    
+
                 });
             commands.CreateCommand("seen")
                 .Description("Returns the date the requested user was last seen")
@@ -104,11 +85,21 @@ namespace BrazilBot
                 .Do(async e =>
                 {
                     User requestedUser = e.Server.FindUsers(e.GetArg("requestedUser")).FirstOrDefault();
-                    if(requestedUser != null)
+                    if (requestedUser != null)
                     {
                         await e.Channel.SendMessage($"{requestedUser.Name} was last seen on {requestedUser.LastActivityAt}.");
                     }
                 });
+            commands.CreateCommand("ShitBot")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Dont blame me, blame the ones writing the code.");
+                });
+            commands.CreateCommand("bouncysheep")
+           .Do(async (e) =>
+           {
+               await e.Channel.SendMessage("The most bounciest bouncer of the bouncy sheep.");
+           });
         }
 
         private void Log(object sender, LogMessageEventArgs e)
